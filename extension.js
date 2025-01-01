b38d89ff9876c175e08f33a586e43dcf990f9cdc
alboxer2000 <20brigidialessandro@icxxsettembrerimini.edu.it>
2025-01-01 19:21:20 +0100

    Create extension.js

---
+++ extension.js
@@ -0,0 +1,29 @@
+(function(Scratch) {
+    'use strict';
+    if (!Scratch.extensions.unsandboxed) {
+      throw new Error('Messages Reader must run unsandboxed');
+    }
+    class messagesread {
+      getInfo() {
+        return {
+          id: 'messagesread',
+          name: 'Messages Reader',
+          blocks: [
+            {
+                opcode: 'getmessage',
+                blockType: Scratch.BlockType.REPORTER,
+                text: 'get messages count from ',
+                arguments: {
+                  getmessage: {
+                    type: Scratch.ArgumentType.STRING,
+                    defaultValue: 'griffpatch'
+                  }
+              },
+            }
+            ]
+        };
+      }
+    // Block Code Here
+    }
+    Scratch.extensions.register(new messagesread());
+  })(Scratch);
